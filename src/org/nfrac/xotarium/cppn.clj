58eb4b4c8a1b5464f9f780eebe82a1fd69d118b8
Felix Andrews <felix@nfrac.org>
2017-01-25 12:33:16 +0800

    Creature body from CPPN
    Muscle and bone.

---
+++ src/org/nfrac/xotarium/cppn.clj
@@ -0,0 +1,248 @@
+(ns org.nfrac.xotarium.cppn
+  (:require [org.nfrac.xotarium.util.algo-graph :as graph]
+            [clojure.spec :as s]
+            [clojure.spec.gen :as gen]))
+
+;; network topology is defined by dependencies between nodes.
+;; so all non-input nodes must have at least one input edge.
+;; and cycles are not allowed.
+(def example-cppn
+  {:inputs #{:bias :x :y :d}
+   :outputs #{:h :s :v}
+   :finals #{:h :s}
+   :nodes {:i0 :gaussian}
+   :edges {:i0 {:d 1.0
+                :y 1.0}
+           :v {:i0 1.0}
+           :h {:i0 1.0}
+           :s {:i0 0.5
+               :v -1.0}}})
+
+(def all-node-types
+  #{:linear :gaussian :sigmoid :sine :sawtooth})
+
+(def auto-node-types
+  #{:linear :gaussian :sigmoid :sine})
+
+(s/def ::node-id (-> any? (s/with-gen #(s/gen ident?))))
+(s/def ::inputs (s/coll-of ::node-id, :min-count 1, :kind set?))
+(s/def ::outputs (s/coll-of ::node-id, :min-count 1, :kind set?))
+(s/def ::nodes (s/map-of ::node-id all-node-types, :min-count 1))
+(s/def ::weight (s/double-in :min -100 :max 100 :NaN? false))
+(s/def ::node-edges (s/map-of ::node-id ::weight))
+(s/def ::edges (s/map-of ::node-id ::node-edges, :min-count 1))
+
+(s/def ::cppn
+  (s/keys :req-un [::inputs
+                   ::outputs
+                   ::nodes
+                   ::edges]))
+
+(defn remap
+  "Transforms a map `m` applying function `f` to each value."
+  [f m]
+  (into (or (empty m) {})
+        (map (fn [[k v]] [k (f v)]))
+        m))
+
+(defn finals
+  [cppn]
+  (or (:finals cppn) (:outputs cppn)))
+
+(defn cppn-graph
+  [cppn]
+  (graph/directed-graph (concat (:inputs cppn)
+                                (keys (:nodes cppn))
+                                (:outputs cppn))
+                        (remap keys (:edges cppn))))
+
+(defn cppn-graph-no-finals
+  [cppn]
+  (let [finals (finals cppn)
+        nodes (concat (:inputs cppn)
+                      (keys (:nodes cppn))
+                      (remove finals (:outputs cppn)))]
+    (graph/directed-graph nodes
+                          (remap keys (apply dissoc (:edges cppn) finals)))))
+
+(defn cppn-strata
+  "A list of sets. The first set contains the only inputs, the last only the
+  final outputs."
+  [cppn]
+  (concat (graph/dependency-list (cppn-graph-no-finals cppn))
+          [(finals cppn)]))
+
+(defn downstream
+  "Returns the collection of all downstream nodes including self."
+  [cppn node-id]
+  (-> (cppn-graph cppn)
+      (graph/reverse-graph)
+      (graph/transitive-closure)
+      (graph/add-loops)
+      (graph/get-neighbors node-id)))
+
+(defn edge-list
+  [cppn]
+  (sort
+   (for [[to m] (:edges cppn)
+         from (keys m)]
+    [to from])))
+
+(defn cppn-weights
+  [cppn]
+  (mapv #(get-in (:edges cppn) %) (edge-list cppn)))
+
+(defn set-cppn-weights
+  [cppn ws]
+  (reduce (fn [cppn [[to from] w]]
+            (assoc-in cppn [:edges to from] w))
+          cppn
+          (map vector (edge-list cppn) ws)))
+
+(defn use-indices
+  [cppn use-node]
+  (keep-indexed (fn [i [to from]]
+                  (when (or (nil? use-node)
+                            (= to use-node))
+                        i))
+                (edge-list cppn)))
+
+(defn rand-skew
+  [max power]
+  (-> (rand (Math/pow max (/ 1 power)))
+      (Math/pow power)))
+
+(defn rand-sign [] (if (pos? (rand-int 2)) 1 -1))
+
+(defn rand-init-weight [] (* (rand-skew 3 2) (rand-sign)))
+
+(def node-prefixes (seq "nmpqrstbcdefgh"))
+
+(defn gen-node-id
+  "Scan through integer range until find usable id.
+  Instead of gensym so as to keep the ids small < 100
+  and avoid collisions with externally provided nodes."
+  [cppn]
+  (loop [pres node-prefixes
+         i 0]
+    (let [pre (first pres)
+          k (keyword (str pre i))]
+      (if (>= i 100)
+        (recur (rest pres) 0)
+        (if (contains? (:nodes cppn) k)
+          (recur pres (inc i))
+          k)))))
+
+(defn mutate-add-node-before
+  [cppn before]
+  (let [type (rand-nth (seq auto-node-types))
+        id (gen-node-id cppn)
+        [from1 w1] (rand-nth (seq (get-in cppn [:edges before])))]
+    (-> cppn
+        (update :nodes assoc id type)
+        (update :edges assoc id {from1 (rand-init-weight)})
+        ;(update-in [:edges before] dissoc from1)
+        (update-in [:edges before] assoc id (rand-init-weight)))))
+
+(defn mutate-add-node
+  [cppn]
+  (mutate-add-node-before cppn (rand-nth (keys (:edges cppn)))))
+
+(defn mutate-append-node
+  [cppn]
+  (mutate-add-node-before cppn (rand-nth (seq (:outputs cppn)))))
+
+(defn mutate-add-conn-to
+  [cppn to-node]
+  (let [to-edges (get (:edges cppn) to-node)
+        candidates (remove (set (concat (keys to-edges)
+                                        (downstream cppn to-node)
+                                        (finals cppn)))
+                           (concat (keys (:nodes cppn))
+                                   (:inputs cppn)
+                                   (:outputs cppn))) ;; outputs ok if not final
+        w (- (rand (* 2.0 2)) 2.0)]
+    (if (seq candidates)
+      (-> cppn
+          (assoc-in [:edges to-node (rand-nth (seq candidates))] w))
+      cppn)))
+
+(defn mutate-add-conn
+  [cppn]
+  (mutate-add-conn-to cppn (rand-nth (keys (:edges cppn)))))
+
+(defn mutate-rewire-conn
+  [cppn]
+  (let [[to-node to-edges] (rand-nth (seq (:edges cppn)))
+        [rm-from old-w] (rand-nth (seq to-edges))
+        cppn2 (mutate-add-conn-to cppn to-node)
+        new-from (-> (apply dissoc (get-in cppn2 [:edges to-node])
+                            (keys to-edges))
+                     keys first)]
+    (if new-from
+      (-> cppn2
+          (update-in [:edges to-node] dissoc rm-from)
+          (assoc-in [:edges to-node new-from] old-w))
+      cppn)))
+
+(defn delete-node
+  [cppn node]
+  (let [above-ed (get-in cppn [:edges node])
+        above (keys above-ed)
+        below (-> (cppn-graph cppn)
+                  (graph/reverse-graph)
+                  (graph/get-neighbors node))]
+    (->
+     (reduce (fn [m below-node]
+               (update-in m [:edges below-node]
+                          (fn [below-ed]
+                            (if (== 1 (count below-ed))
+                              ;; node would be orphaned
+                              (select-keys above-ed [(rand-nth above)])
+                              (dissoc below-ed node)))))
+             cppn
+             below)
+     (update :edges dissoc node)
+     (update :nodes dissoc node))))
+
+(defn link-nodes
+  "Attempt to link node a -> b,
+   but if that would be cyclic, link b -> a instead."
+  [cppn node-a node-b]
+  (if (or (contains? (finals cppn) node-a)
+          (contains? (:inputs cppn) node-b)
+          (contains? (downstream cppn node-b) node-a))
+    (assoc-in cppn [:edges node-a node-b] 1.0)
+    (assoc-in cppn [:edges node-b node-a] 1.0)))
+
+(defn remove-edge
+  [cppn from to]
+  (update-in cppn [:edges to]
+             (fn [m]
+               (let [m (dissoc m from)]
+                 ;; ensure all nodes have at least one input
+                 (if (empty? m)
+                   (assoc m (rand-nth (seq (:inputs cppn))) 1.0)
+                   m)))))
+
+(defn interp
+  [from to z]
+  (+ from (* z (- to from))))
+
+(defn rand-weight
+  [from-w perturbation]
+  (let [global-w (rand-init-weight)
+        locally (+ from-w (* perturbation 0.5 global-w))
+        globally (interp from-w global-w (* perturbation perturbation))]
+    (+ (* perturbation globally)
+       (* (- 1.0 perturbation) locally))))
+
+(defn randomise-weights
+  [cppn perturbation use-node]
+  (let [ws (cppn-weights cppn)
+        use-is (use-indices cppn use-node)
+        new-ws (reduce (fn [ws i]
+                         (assoc ws i (rand-weight (nth ws i) perturbation)))
+                       ws
+                       use-is)]
+    (set-cppn-weights cppn new-ws)))
---
+++ src/org/nfrac/xotarium/run.clj
@@ -0,0 +1,249 @@
+(ns org.nfrac.xotarium.run
+  (:require [org.nfrac.xotarium.cppn :as cppn]
+            [org.nfrac.xotarium.cppn-compile :as cc]
+            [org.nfrac.xotarium.plant-cave :as cave]
+            [org.nfrac.liquidfun.testbed :as bed]
+            [org.nfrac.liquidfun.core :as lf :refer [body! joint!
+                                                     particle-system!]]
+            [org.nfrac.liquidfun.vec2d :as v2d]
+            [org.nfrac.xotarium.util.algo-graph :as graph]
+            [quil.core :as quil :include-macros true]
+            [quil.middleware]
+            [clojure.pprint]
+            [clojure.spec :as s])
+  (:import (org.bytedeco.javacpp
+            liquidfun$b2ContactListener
+            liquidfun$b2ParticleBodyContact
+            liquidfun$b2ParticleContact
+            liquidfun$b2World
+            liquidfun$b2Body
+            liquidfun$b2Transform
+            liquidfun$b2Vec2
+            liquidfun$b2ParticleSystem
+            liquidfun$b2ParticleGroup
+            liquidfun$b2ParticleDef
+            liquidfun$b2QueryCallback)))
+
+;; interpretation:
+;; if muscle is positive, express muscle.
+;;   - if bone is also positive, the muscle binds to adjacent bone.
+;; if bone is positive (muscle is not), express bone.
+;; otherwise (both non-positive), empty space.
+
+(def seed-cppn
+  {:inputs #{:bias :x :y :d}
+   :outputs #{:bone :muscle :phase}
+   :finals #{:bone :phase}
+   :nodes {:i0 :gaussian}
+   :edges {:i0 {:d -1.0}
+           :muscle {:i0 1.0
+                    :bias 0.2}
+           :bone {:d -1.0
+                  :bias -0.1}
+           :phase {;:muscle 1.0
+                   :x 1.0}}})
+
+(defn hex-neighbours
+  "Returns hexagonal coordinates linked to their immediate neighbours.
+  Ids are like [ix iy] which are integer or fractional (1/2) steps.
+
+  Uses a hex grid: every odd row is offset by 1/2 in x.
+  Below, the x's neighbours are marked *.
+```
+  o * * o
+   * x *
+  o * * o
+   o o o
+```"
+  [nx ny]
+  (into
+   {}
+   (for [iy (range 0 ny)
+         ;; every second row offset by 1/2
+         ix (range (if (even? iy) 0 1/2) nx)]
+     [[ix iy] (filterv (fn [[jx jy]]
+                         (and (<= 0 jx) (< jx nx)
+                              (<= 0 jy) (< jy ny)))
+                       [[(dec ix) iy]
+                        [(inc ix) iy]
+                        [(- ix 1/2) (dec iy)]
+                        [(+ ix 1/2) (dec iy)]
+                        [(- ix 1/2) (inc iy)]
+                        [(+ ix 1/2) (inc iy)]])])))
+
+(defn filter-graph
+  "Filters the graph to nodes (expressed? id) and edges (connected? id1 id2)."
+  [g-nbs expressed? connected?]
+  (loop [ids (keys g-nbs)
+         nbs {}]
+    (if-let [id (first ids)]
+      (if (expressed? id)
+        (let [i-nbs (filter #(connected? id %) (get g-nbs id))]
+          (recur (rest ids) (assoc nbs id i-nbs)))
+        ;; not expressed
+        (recur (rest ids) nbs))
+      ;; done
+      nbs)))
+
+(defn tissue-groups
+  "Returns groups of particle coordinates, partitioned into each contiguous
+  bone, and the remaining muscle partitioned between binding and inert."
+  [hex-g pheno]
+  (let [expressed? (fn [id]
+                     (let [m (pheno id)]
+                       (or (pos? (:bone m)) (pos? (:muscle m)))))
+        muscle? (fn [id]
+                  (pos? (:muscle (pheno id))))
+        bone? (fn [id]
+                (and (expressed? id) (not (muscle? id))))
+        bone-g (filter-graph hex-g bone? #(and (bone? %1) (bone? %2)))
+        bones (graph/scc (graph/directed-graph (keys bone-g) bone-g))
+        muscle-ids (filter muscle? (keys pheno))
+        muscle-inert (set (remove #(pos? (:bone (pheno %))) muscle-ids))
+        muscle-binding (set (remove muscle-inert muscle-ids))]
+    (concat
+     (map vector (repeat :bone) bones)
+     [[:muscle-inert muscle-inert]
+      [:muscle-binding muscle-binding]])))
+
+(defn morpho-data
+  "Calculates the expressed tissues and returns the blueprint for the body.
+  Returns a sequence of groups, each containing a set of coordinates of one
+  tissue type. Contiguous bones are returned as separate groups.
+  Tissue is one of :bone, :muscle-binding, :muscle-inert.
+  Muscle particles have an associated phase attribute."
+  [cppn-fn p-radius [cx cy] [w h]]
+  (let [stride (* p-radius 2.0 0.75)
+        nx (quot w stride)
+        ny (quot h stride)
+        hex-g (hex-neighbours nx ny)
+        x0 (- cx (/ w 2))
+        y0 (- cy (/ h 2))
+        coord (fn [[ix iy]] [(+ x0 (* ix stride))
+                             (+ y0 (* iy stride))])
+        pheno (into
+               {}
+               (map (fn [id]
+                      (let [[ix iy] id
+                            zx (-> (+ ix 0.5) (/ nx) (- 0.5) (* 2))
+                            zy (-> (+ iy 0.5) (/ ny) (- 0.5) (* 2))
+                            d (cc/xy->d zx zy)
+                            bias 1.0]
+                        [id (cppn-fn bias d zx zy)])))
+               (keys hex-g))
+        tgroups (tissue-groups hex-g pheno)]
+    (map (fn [[tissue ids]]
+           {:tissue tissue
+            :coords (map coord ids)
+            :phases (map #(:phase (pheno %)) ids)})
+         tgroups)))
+
+
+(s/def ::tissue #{:bone :muscle-binding :muscle-inert})
+
+(s/def ::coord (s/tuple double? double?))
+(s/def ::coords (s/every ::coord))
+
+(s/def ::tissue-group
+  (s/keys :req-un [::tissue
+                   ::coords
+                   ::phases]))
+
+(s/fdef morpho-data
+        :args (s/cat :cppn-fn ::cppn-fn
+                     :p-radius double?
+                     :center ::coord
+                     :size (s/tuple double? double?))
+        :ret (s/coll-of ::tissue-group))
+
+(def tissue-color
+  {:bone [255 255 255 255]
+   :muscle-binding [255 128 128 255]
+   :muscle-inert [255 0 0 255]})
+
+(defn morpho-construct
+  [mdata ^liquidfun$b2World world ^liquidfun$b2ParticleSystem ps]
+  (doall
+    (for [{:keys [tissue coords phases]} mdata]
+      (let [pdata (lf/seq->v2arr coords)]
+        (lf/particle-group!
+         ps
+         {:flags (lf/particle-flags
+                  (case tissue
+                   :bone #{}
+                   :muscle-inert #{:elastic}
+                   :muscle-binding #{:elastic :reactive}))
+          :group-flags (lf/particle-group-flags
+                        (case tissue
+                          :bone #{:rigid :solid}
+                          (:muscle-inert :muscle-binding) #{:solid}))
+          :color (tissue-color tissue)
+          :position-data pdata
+          :particle-count (count coords)})))))
+
+(defn setup
+  []
+  (let [world (cave/build-world)
+        ps (first (lf/particle-sys-seq world))
+        p-radius (.GetRadius ps)
+        cppn seed-cppn
+        cppn-fn (cc/build-cppn-fn cppn)
+        mdata (morpho-data cppn-fn p-radius [0 0] [1.0 1.0])
+        pgs (morpho-construct mdata world ps)]
+    (assoc bed/initial-state
+           :world world
+           :particle-system ps
+           :particle-iterations 3
+           :camera (bed/map->Camera {:width 6 :height 6 :center [0 0]}))))
+
+(defn step
+  [state]
+  (bed/world-step state))
+
+(defn draw
+  [state]
+  (bed/draw state true)
+  (let []
+    (quil/fill 255)
+    (quil/text (str "Keys: (g) toggle gravity (d) damping (b) ball"
+                    " (a) air")
+               10 10)))
+
+(defn my-key-press
+  [state event]
+  (let [ps ^liquidfun$b2ParticleSystem (:particle-system state)]
+    (case (:key event)
+      :a (cave/do-add-air state)
+      :b (do
+           (body! (:world state) {}
+                  {:shape (lf/circle 0.25)
+                   :restitution 0.1
+                   :density 1.0})
+           state)
+      :g (do
+           (.SetGravityScale ps (if (== 1.0 (.GetGravityScale ps))
+                                  0.0 1.0))
+           state)
+      :d (do
+           (.SetDamping ps (if (== 1.0 (.GetDamping ps))
+                             0.0 1.0))
+           state)
+      ;; otherwise pass on to testbed
+      (bed/key-press state event))))
+
+(defn run
+  [& args]
+  (quil/sketch
+      :title "Xotarium"
+      :host "liquidfun"
+      :setup setup
+      :update (fn [s] (if (:paused? s) s (step s)))
+      :draw draw
+      :key-typed my-key-press
+      :mouse-pressed bed/mouse-pressed
+      :mouse-released bed/mouse-released
+      :mouse-dragged bed/mouse-dragged
+      :mouse-wheel bed/mouse-wheel
+      :size [600 500]
+      :features [:resizable]
+      :middleware [quil.middleware/fun-mode]))
