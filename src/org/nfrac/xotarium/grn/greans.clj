956cc7a487684816764fdfe57f48a2794fa451c9
Felix Andrews <felix@nfrac.org>
2017-02-17 10:57:11 +0800

    Simplify GReaNs data model

--- src/org/nfrac/xotarium/grn/greans.clj
+++ src/org/nfrac/xotarium/grn/greans.clj
@@ -5,40 +5,54 @@
   (:require [clojure.spec :as s]
             [clojure.spec.gen :as gen]))
 
+(def INIT_MAX_COORD 5.0)
+
+(def parameter-defaults
+  {})
+
 ;;; evolvable form of genome
 
-(def element-types #{::signal ::promoter ::gene})
+(def element-types #{::promoter ::gene})
+
+(s/def ::element-type element-types)
+
+(s/def ::sign #{-1 1})
 
 (s/def ::coordinate
   (-> (s/double-in :infinite? false :NaN? false)
-      (s/with-gen #(gen/double* {:min 0.0 :max 10.0 :NaN? false}))))
+      (s/with-gen #(gen/double* {:min 0.0 :max INIT_MAX_COORD :NaN? false}))))
+
+(s/def ::coords (s/coll-of ::coordinate :count 2))
 
 (s/def ::element
-  (s/cat :type element-types
-         :sign #{-1 1}
-         :coords (s/coll-of ::coordinate :count 2)))
-
-(s/def ::signal
-  (s/and ::element #(= (:type %) ::signal)))
+  (s/keys :req [::element-type
+                ::sign
+                ::coords]))
 
 (s/def ::promoter
-  (s/and ::element #(= (:type %) ::promoter)))
+  (s/and ::element #(= (::element-type %) ::promoter)))
 
 (s/def ::gene
-  (s/and ::element #(= (:type %) ::gene)))
+  (s/and ::element #(= (::element-type %) ::gene)))
 
 (s/def ::regulatory-unit
   (s/cat :promoters (s/+ ::promoter)
          :genes (s/+ ::gene)))
 
+(s/def ::elements
+  (s/+ ::regulatory-unit))
+
+(s/def ::output-tfs (s/coll-of nat-int?))
+(s/def ::input-tfs (s/coll-of nat-int?))
+
 (s/def ::parameters
   (s/keys))
 
 (s/def ::genome
-  (s/cat :signals (s/spec (s/+ ::signal))
-         :internal (s/spec (s/+ (s/spec ::regulatory-unit)))
-         :outputs (s/coll-of nat-int?)
-         :params ::parameters))
+  (s/keys :req [::elements
+                ::input-tfs
+                ::output-tfs
+                ::parameters]))
 
 (comment
   (s/exercise ::gene)
@@ -46,9 +60,9 @@
 
 (defn random-element
   [type]
-  [type
-   (rand-nth [-1 1])
-   [(rand 5.0) (rand 5.0)]])
+  {::element-type type
+   ::sign (rand-nth [-1 1])
+   ::coords [(rand INIT_MAX_COORD) (rand INIT_MAX_COORD)]})
 
 (defn random-unit
   []
@@ -59,11 +73,14 @@
 
 (defn random-genome
   [n-in n-out]
-  (let [n-units (+ n-out (rand-int 3))]
-    [(repeatedly n-in #(random-element ::signal))
-     (repeatedly n-units #(random-unit))
-     (repeatedly n-out #(rand-int 100))
-     {}]))
+  (let [n-units (+ n-in n-out 1)
+        es (apply concat (repeatedly n-units #(random-unit)))
+        n-tfs (count (filter #(= ::gene (::element-type %)) es))
+        tf-ids (shuffle (range n-tfs))]
+    {::elements es
+     ::input-tfs (take n-in tf-ids)
+     ::output-tfs (take n-out (drop n-in tf-ids))
+     ::parameters parameter-defaults}))
 
 ;;; phenotype / usable form
 
@@ -98,6 +115,8 @@
                 ::unit-tfs
                 ::influences
                 ::concs
+                ::input-tfs
+                ::output-tfs
                 ::parameters]))
 
 (defn index-counts
@@ -117,10 +136,10 @@
 
 (defn promoter-influences
   [pr tfs]
-  (let [pr-coords (:coords pr)]
+  (let [pr-coords (::coords pr)]
     (reduce (fn [m [tf-id tf]]
-              (if-let [aff (affinity pr-coords (:coords tf))]
-                (assoc m tf-id (* aff (:sign pr) (:sign tf)))
+              (if-let [aff (affinity pr-coords (::coords tf))]
+                (assoc m tf-id (* aff (::sign pr) (::sign tf)))
                 m))
             {}
             (map-indexed vector tfs))))
@@ -128,21 +147,20 @@
 (defn genome->cell
   [genome]
   (let [cg (s/conform ::genome genome)
-        units (:internal cg)
-        sigs (:signals cg)
+        units (::elements cg)
         unit-promoters (index-counts (map #(count (:promoters %)) units) 0)
-        unit-tfs (index-counts (map #(count (:genes %)) units) (count sigs))
+        unit-tfs (index-counts (map #(count (:genes %)) units) 0)
         all-prs (mapcat :promoters units)
-        all-tfs (concat sigs (mapcat :genes units))
+        all-tfs (mapcat :genes units)
         ;; pr -> {tf -> infl}
         pr-influences (mapv #(promoter-influences % all-tfs) all-prs)
         ]
-    {::unit-promoters unit-promoters
-     ::unit-tfs unit-tfs
-     ::influences pr-influences
-     ::concs (vec (repeat (count all-tfs) 0.0))
-     ::outputs (:outputs cg)
-     ::parameters (:params cg)}))
+    (merge
+     (select-keys cg [::input-tfs ::output-tfs ::parameters])
+     {::unit-promoters unit-promoters
+      ::unit-tfs unit-tfs
+      ::influences pr-influences
+      ::concs (vec (repeat (count all-tfs) 0.0))})))
 
 (defn promoter-activation
   [pr-id influences concs]
@@ -168,23 +186,26 @@
           (+ 1.0 e))
        conc)))
 
-(defn merge-vectors
-  [v v2]
+(defn vector-merge
+  [v kvs]
   (persistent!
-   (reduce-kv assoc! (transient v) v2)))
+   (reduce-kv assoc! (transient v) kvs)))
 
 (defn step
-  [cell signal-concs dt]
+  [cell input-concs dt]
   (let [dt (double dt)
-        concs (merge-vectors (::concs cell) signal-concs)
+        n-tfs (count (::concs cell))
+        concs (vector-merge (::concs cell)
+                            (zipmap (map #(mod % n-tfs) (::input-tfs cell))
+                                    input-concs))
         influences (::influences cell)
-        promoters (::unit-promoters cell)
+        unit-promoters (::unit-promoters cell)
         unit-tfs (::unit-tfs cell)
         n-units (count unit-tfs)]
     (loop [unit-i 0
            new-concs (transient concs)]
       (if (< unit-i n-units)
-        (let [pr-ids (get promoters unit-i)
+        (let [pr-ids (get unit-promoters unit-i)
               unit-a (max 0.0
                           (transduce
                            (map #(promoter-activation % influences concs))
@@ -206,9 +227,8 @@
   (let [concs (::concs cell)
         n (count concs)]
     (mapv #(get concs (mod % n))
-          (::outputs cell))))
+          (::output-tfs cell))))
 
-;; TODO: mutation etc
 ;; TODO: rng
 
 (comment
@@ -218,3 +238,5 @@
   (def csteps (iterate #(step % [1.0 1.0] 0.05) cell))
   (map println (map cell-outputs (take 10 csteps)))
 )
+
+;;; mutation etc
