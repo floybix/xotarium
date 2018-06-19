c70c9debe0b7cca6b896bc1562870f5a435798d6
Felix Andrews <felix@nfrac.org>
2018-06-19 14:57:07 +1000

    Use tools.deps

--- project.clj
+++ project.clj
@@ -3,14 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-
-  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
-                 [org.clojure/core.async "0.3.442"]
-                 [org.clojure/test.check "0.9.0"]
-                 [org.nfrac/liquidfun-clj "0.1.0-SNAPSHOT"]
-                 [org.nfrac/liquidfun-clj.testbed "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
 
   :aot [org.nfrac.xotarium.plant-cave]
   :jvm-opts ^:replace ["-server" "-Dclojure.compiler.direct-linking=true"]
-
-  :profiles {:dev {:dependencies [[gyptis "0.2.2"]]}})
+  )
